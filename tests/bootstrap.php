1487f2682beed16bf3c6584c6b5acba1dcea12e9
stemeda <stephan@62.157.140.133>
2016-09-15 21:11:44 +0200

    use database config from env

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,10 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+
 use Cake\Datasource\ConnectionManager;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
+ConnectionManager::drop('test');
 ConnectionManager::config('test', ['url' => getenv('db_dsn')]);
