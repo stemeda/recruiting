afcc990555cac03730e37fa7664e68431618e12d
stemeda <stephan@62.157.140.133>
2016-10-27 14:20:59 +0200

    composer update, cancellation shell

--- src/Shell/CancellationShell.php
+++ src/Shell/CancellationShell.php
@@ -1,7 +1,11 @@
 <?php
 namespace App\Shell;
 
+use App\Model\Entity\Application;
+use App\Model\Entity\Position;
 use Cake\Console\Shell;
+use Cake\Mailer\Email;
+use Cake\ORM\TableRegistry;
 
 /**
  * Cancellation shell command.
@@ -30,6 +34,93 @@ class CancellationShell extends Shell
      */
     public function main()
     {
-        $this->out($this->OptionParser->help());
+        $applications = TableRegistry::get('Applications')
+            ->find('all')
+            ->contain('ApplicationsPositionDescriptionValues')
+            ->contain('ApplicationStatus')
+            ->contain('Candidates.CandidatesCandidateDescriptionValues')
+            ->contain('Candidates.Users')
+            ->where([
+                'ApplicationStatus.closes_application' => false
+            ]);
+
+        $valuesNotAllNeestedSet = [];
+        foreach ($applications as $applicationKey => $application) {
+            $result = $this->_calculateApplicationValue($application);
+            if (!$result['allNeededSet']) {
+                $valuesNotAllNeestedSet[$applicationKey] = $result;
+            }
+        }
+        foreach ($valuesNotAllNeestedSet as $cancelApplication) {
+            $data = [
+                'application_status_id' => 2,
+            ];
+            $application = TableRegistry::get('Applications')->patchEntity($cancelApplication['application'], $data);
+
+            if (!TableRegistry::get('Applications')->save($application)) {
+                $this->err('Application #' . $application->id . ' could not be canceled!');
+            } else {
+                $email = new Email();
+                $t = $email->template('cancel', 'default')
+                    ->emailFormat('both')
+                    ->to($application->candidate->user->email)
+                    ->subject('Absage Bewerbung')
+                    ->send();
+            }
+        }
+    }
+
+    /**
+     * calculate the value of an application out of the position.
+     * returns the main Information of the position and the value
+     *
+     * @param Application $application the application
+     * @return array
+     */
+    protected function _calculateApplicationValue(Application $application)
+    {
+        $position = TableRegistry::get('Positions')->get(
+            $application->positions_id,
+            [
+                'contain' => [
+                    'PositionsPositionDescriptionValues',
+                    'PositionsCandidateDescriptionValues',
+                    ]
+            ]
+        );
+        $result = [
+            'value' => 0,
+            'application' => $application,
+            'position' => $position,
+            'allNeededSet' => true,
+        ];
+        foreach ($position->positions_candidate_description_values as $key => $positionsCandidateDescriptionValue) {
+            $candidatesCandidateDescriptionValues = $application->candidate->candidates_candidate_description_values;
+            $found = false;
+            foreach ($candidatesCandidateDescriptionValues as $candidatesCandidateDescriptionValue) {
+                if ($candidatesCandidateDescriptionValue->candidate_description_values_id === $positionsCandidateDescriptionValue->candidate_description_values_id) {
+                    $found = true;
+                    $result['value'] += $positionsCandidateDescriptionValue->importance;
+                }
+                if (!$found && $positionsCandidateDescriptionValue->needed) {
+                    $result['allNeededSet'] = false;
+                }
+            }
+        }
+        foreach ($position->positions_position_description_values as $key => $positionsPositionDescriptionValues) {
+            $applicationPositionDescriptionValues = $application->applications_position_description_values;
+            $found = false;
+            foreach ($applicationPositionDescriptionValues as $applicationPositionDescriptionValue) {
+                if ($applicationPositionDescriptionValue->positions_description_values_id === $positionsPositionDescriptionValues->positions_description_values_id) {
+                    $found = true;
+                    $result['value'] += $positionsPositionDescriptionValues->importance;
+                }
+                if (!$found && $positionsPositionDescriptionValues->needed) {
+                    $result['allNeededSet'] = false;
+                }
+            }
+        }
+
+        return $result;
     }
 }
---
+++ src/Template/Email/text/cancel.ctp
@@ -0,0 +1,4 @@
+Absage Ihrer Bewerbung
+
+
+Nach intensiver Prüfung Ihrer Bewerbungsunterlagen müssen wir Ihnen leider Absagen.
